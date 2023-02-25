8289b24123064388876bd662b8e99f2e13a3a59c
egor <egoreq@bk.ru>
2023-02-25 23:26:03 +0400

    fix: fix exp tokens

--- pet-authentication/src/service/tokenService/generateTokens.js
+++ pet-authentication/src/service/tokenService/generateTokens.js
@@ -4,10 +4,10 @@ import { JWT_ACCESS_SECRET, JWT_REFRESH_SECRET } from "../../constants.js";
 
 export const generateTokens = (payload) => {
   const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, {
-    expiresIn: "30s",
+    expiresIn: "30m",
   });
   const refreshToken = jwt.sign(payload, JWT_REFRESH_SECRET, {
-    expiresIn: "2m",
+    expiresIn: "30d",
   });
   return {
     accessToken,
